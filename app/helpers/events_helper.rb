6588b6db844eed250363ba12f9313ee2afb1d707
cndaniel <cndaniel@me.com>
2017-03-05 19:33:15 +0100

    fefine table layout

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,14 @@
 module EventsHelper
   require 'chronic'
- 
 
 
+  def render_shift(event)
+    if current_user
+      link_to event.summary, edit_event_path(event)
+    else
+      event.summary
+    end
+  end
   def set_up_shift_time(chronic_time)
     Chronic.parse(chronic_time).strftime('%Y-%m-%d %H:%M:%S')
 
